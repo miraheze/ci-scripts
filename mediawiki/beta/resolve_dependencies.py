da509c369fd2d71d947a2ae16496578540cc26e0
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-20 19:05:21 -0600

    Test yaml loading

---
+++ mediawiki/beta/resolve_dependencies.py
@@ -0,0 +1,24 @@
+# A script to resolve dependencies of MediaWiki extension for Quibble test
+import yaml
+
+from os import environ
+# pf for https://raw.githubusercontent.com/wikimedia/integration-config/master/zuul/parameter_functions.py
+from pf import dependencies, get_dependencies
+
+if 'MEDIAWIKI_VERSION' in environ and environ['MEDIAWIKI_VERSION'] == 'REL1_35':
+  dependencies['EventLogging'].remove('EventBus')
+
+# Add dependencies of target extension
+with open('dependencies.yaml', 'r') as f:
+    dependencies['ext'] = yaml.load(f)
+
+# Resolve
+resolvedDependencies = []
+for d in get_dependencies('ext', dependencies):
+  # Skip parsoid which is a virtual extension
+  if d == 'parsoid':
+    continue
+  d = 'mediawiki/extensions/' + d
+  d = d.replace('/extensions/skins/', '/skins/')
+  resolvedDependencies.append(d)
+print(' '.join(resolvedDependencies))

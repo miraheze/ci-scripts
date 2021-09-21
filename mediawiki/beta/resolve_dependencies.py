39cecfdd05bac397182b82782e387f358b7f04ee
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-20 19:34:30 -0600

    Update resolve_dependencies.py

--- mediawiki/beta/resolve_dependencies.py
+++ mediawiki/beta/resolve_dependencies.py
@@ -21,4 +21,4 @@ for d in get_dependencies('ext', dependencies):
   d = 'mediawiki/extensions/' + d
   d = d.replace('/extensions/skins/', '/skins/')
   resolvedDependencies.append(d)
-print(' '.join(resolvedDependencies) + get_dependencies('ext', dependencies)[d]['branch'])
+print(' '.join(resolvedDependencies) + dependencies['ext'][d]['branch'])

0dfbe18932d3512634ff2af9a21600fbd38398ac
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-20 21:29:45 -0600

    Use YAML syntax for dependancies.
    In order to allow setting dependencies for other repos, not in Wikimedia Gerrit.

--- mediawiki/resolve_dependencies.py
+++ mediawiki/resolve_dependencies.py
@@ -1,4 +1,5 @@
 # A script to resolve dependencies of MediaWiki extension for Quibble test
+import yaml
 
 from os import environ
 # pf for https://raw.githubusercontent.com/wikimedia/integration-config/master/zuul/parameter_functions.py
@@ -8,15 +9,26 @@ if 'MEDIAWIKI_VERSION' in environ and environ['MEDIAWIKI_VERSION'] == 'REL1_35':
   dependencies['EventLogging'].remove('EventBus')
 
 # Add dependencies of target extension
-dependencies['ext'] = open('dependencies').read().splitlines()
+with open('dependencies', 'r') as f:
+    dependencies['ext'] = yaml.load(f, Loader=yaml.SafeLoader)
 
 # Resolve
 resolvedDependencies = []
 for d in get_dependencies('ext', dependencies):
+  repo = ''
+  branch = ''
+  if 'repo' in dependencies['ext'][d]:
+    if dependencies['ext'][d]['repo'] not in ('auto','default'):
+      repo = '|' + dependencies['ext'][d]['repo']
+    if 'branch' in dependencies['ext'][d]:
+      if dependencies['ext'][d]['branch'] not in ('auto','default'):
+        branch = '|' + dependencies['ext'][d]['branch']
+
   # Skip parsoid which is a virtual extension
   if d == 'parsoid':
     continue
   d = 'mediawiki/extensions/' + d
   d = d.replace('/extensions/skins/', '/skins/')
+  d = d + repo + branch
   resolvedDependencies.append(d)
 print(' '.join(resolvedDependencies))

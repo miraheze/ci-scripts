07e1b77c8b878d10ea0ecad044bc4329ae7d53fa
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-23 13:23:57 -0600

    Try using raw sql

--- mediawiki/globals/setup-CreateWiki.php
+++ mediawiki/globals/setup-CreateWiki.php
@@ -1,15 +1,11 @@
 <?php
 
 use MediaWiki\MediaWikiServices;
-use Wikimedia\Rdbms\DBQueryError;
+use Wikimedia\Rdbms\Database;
 
 $wgWikimediaJenkinsCI = true;
 
 define( 'CW_DB', 'wikidb' );
-
-if ( !defined( 'MW_PHPUNIT_TEST' ) ) {
-	define( 'MW_PHPUNIT_TEST', true );
-}
 
 require_once "$IP/extensions/CreateWiki/includes/WikiInitialise.php";
 $wi = new WikiInitialise();
@@ -48,40 +44,19 @@ $wgManageWikiDatabase = 'wikidb';
 $wgHooks['MediaWikiServices'][] = 'insertWiki';
 
 function insertWiki( MediaWikiServices $services ) {
-	try {
-		$dbw = wfInitDBConnection();
+	$db = wfInitDBConnection();
 
-		$dbw->insert(
-			'cw_wikis',
-			[
-				'wiki_dbname' => 'wikidb',
-				'wiki_dbcluster' => 'c1',
-				'wiki_sitename' => 'TestWiki',
-				'wiki_language' => 'en',
-				'wiki_private' => (int)0,
-				'wiki_creation' => $dbw->timestamp(),
-				'wiki_category' => 'uncategorized',
-				'wiki_closed' => (int)0,
-				'wiki_deleted' => (int)0,
-				'wiki_locked' => (int)0,
-				'wiki_inactive' => (int)0,
-				'wiki_inactive_exempt' => (int)0,
-				'wiki_url' => 'http://127.0.0.1:9412'
-			],
-			__METHOD__,
-			[ 'IGNORE' ]
-		);
-
-		$services->resetServiceForTesting( 'DBLoadBalancer' );
-	} catch ( DBQueryError $e ) {
-		return;
-	}
+	$db->begin();
+	$db->query( 'USE wikidb;' );
+	$db->query( 'SOURCE ' . __DIR__ . '/../sql/add-wiki.sql;' );
+	$db->commit();
 }
 
 function wfInitDBConnection() {
-	return MediaWikiServices::getInstance()
-		->getDBLoadBalancer()
-		->getMaintenanceConnectionRef( DB_PRIMARY );
+	return Database::factory( 'mysql', [
+		'host' => $GLOBALS['wgDBserver'],
+		'user' => 'root',
+	] );
 }
 
 $wi->readCache();
---
+++ mediawiki/sql/add-wiki.sql
@@ -0,0 +1,29 @@
+INSERT IGNORE INTO cw_wikis (
+    wiki_dbname,
+    wiki_dbcluster,
+    wiki_sitename,
+    wiki_language,
+    wiki_private,
+    wiki_creation,
+    wiki_category,
+    wiki_closed,
+    wiki_deleted,
+    wiki_locked,
+    wiki_inactive,
+    wiki_inactive_exempt,
+    wiki_url
+) VALUES (
+    'wikidb',
+    'c1',
+    'TestWiki',
+    'en',
+    0,
+    DATE_FORMAT( FROM_UNIXTIME( UNIX_TIMESTAMP() ), '%Y%m%d%H%i%s' ),
+    'uncategorized',
+    0,
+    0,
+    0,
+    0,
+    0,
+    'http://127.0.0.1:9412'
+)
